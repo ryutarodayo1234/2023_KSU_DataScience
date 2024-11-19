ac1d1e4ff8cdfd1946e030666f1cd23cb9ce5e12
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2024-11-19 10:15:36 +0900

    Add files via upload

--- 2024_communicationNetworkExercise/javascript_map/script.js
+++ 2024_communicationNetworkExercise/javascript_map/script.js
@@ -9,24 +9,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// 国土地理院
-// L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/pale/{z}/{x}/{y}.png', {
-//   maxZoom: 18,
-//   attribution: '<a href="https://maps.gsi.go.jp/development/ichiran.html" target="_blank">国土地理院</a>',
-// }).addTo(map);
-
-//アイコン
-// const whiteIcon = L.icon({
-//   iconUrl: 'ico.png',
-//   shadowUrl: 'ico_shadow.png',
-// 
-// iconSize:     [40, 40], // size of the icon
-// shadowSize:   [40, 40], // size of the shadow
-// iconAnchor:   [20, 40], // point of the icon which will correspond to marker's location
-// shadowAnchor: [20, 40],  // the same for the shadow
-// popupAnchor:  [0, 42] // point from which the popup should open relative to the iconAnchor
-// });
-
 //複数アイコンをまとめて定義
 const circleIcon = L.Icon.extend({
   options: {
@@ -49,3 +31,15 @@ L.marker([51.55321688137271, -0.16959787005461097], { icon: whiteIcon }).addTo(m
 
 L.marker([51.547546580277, -0.16719353787558758], { icon: pinkIcon }).addTo(map)
   .bindPopup('A pretty CSS popup.<br> Easily customizable.');
+
+  // クリック位置の緯度経度表示
+const popup = L.popup();
+
+function onMapClick(e) {
+  popup
+    .setLatLng(e.latlng)
+    .setContent("ここは" + e.latlng.toString() + "です")
+    .openOn(map);
+}
+
+map.on('click', onMapClick);
---
+++ 2024_communicationNetworkExercise/javascript_scroll/script.js
@@ -0,0 +1,14 @@
+// JavaScript
+const show = (entries) => {
+  const keyframes = {
+    opacity: [0, 1],
+    translate: ['200px 0',0]
+  }
+  entries[0].target.animate(keyframes,600);
+}
+
+const observer = new IntersectionObserver(show);
+
+observer.observe(document.querySelector('#img01'));
+observer.observe(document.querySelector('#img02'));
+observer.observe(document.querySelector('#img03'));
